07003b4abd099d77770f5ed59a1aacc66b4cd3cc
Jerome Fitzgerald <jeromes@gmail.com>
2021-02-28 10:31:11 -0500

    ci: 🎡 switch to @semantic-release/release-notes-generator, test
    .

--- release.config.js
+++ release.config.js
@@ -1,18 +1,34 @@
+/* eslint-disable object-property-newline */
 const isCI = require('is-ci');
 // eslint-disable-next-line global-require, no-unused-expressions
 !isCI && require('dotenv').config({path: './.env'});
 
 module.exports = {
   branches: [
-    // eslint-disable-next-line object-property-newline
     {name: 'main', prerelease: 'main'},
     {name: 'master'},
-    // eslint-disable-next-line object-property-newline
     {name: 'canary', prerelease: 'canary'}
   ],
   extends: [],
   plugins: [
-    '@semantic-release/changelog',
+    // @note(deprecate) move to release notes for multi-channel releases
+    // '@semantic-release/changelog',
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING']
+        }
+      }
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING']
+        }
+      }
+    ],
     '@semantic-release/npm',
     '@semantic-release/git'
   ]
